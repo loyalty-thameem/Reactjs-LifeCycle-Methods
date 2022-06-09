5ca605b6c09baa182bb7d48154504fdddbdda080
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-09 15:01:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mrbzhm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mrbzhm)
---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './style.css';
+
+export class ChildComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Constructor Method',
+    };
+    console.log('Constructor');
+  }
+  // we are changing our initial state of name inside the constructor method to props which we are receiving in getDerivedStateFromProps
+  static getDerivedStateFromProps(props, state) {
+    console.log('getDerivedStateFromProps Method');
+    return {
+      name: props.nameFromParent,
+    };
+  }
+
+  render() {
+    console.log('This method is responsible to render our JSX to DOM');
+    const name = this.state.name;
+    return (
+      <div>
+        <p>This is {name} </p>
+      </div>
+    );
+  }
+}
+export default class App extends React.Component {
+  render() {
+    console.log('Render from parent');
+    return (
+      <div>
+        <ChildComponent nameFromParent="getDerivedStateFromProps Method" />
+      </div>
+    );
+  }
+}

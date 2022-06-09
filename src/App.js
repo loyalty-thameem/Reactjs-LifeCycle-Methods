3ee6bc5c3e81b97093ee6c36866fdd3ed8e891ba
loyalty-thameem <loyalty.thameem@gmail.com>
2022-06-09 16:18:48 +0530

    end of update. componentDidUpdate method.

--- src/App.js
+++ src/App.js
@@ -5,35 +5,27 @@ export class ChildComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: 'Constructor Method',
+      name: 'from previous state',
     };
     console.log('Constructor');
   }
 
-  shouldComponentUpdate() {
-    console.log('shouldComponentUpdate Method');
-    return true; //Change to false for state to no update
-  }
   // handle
   componentDidMount() {
     console.log('componentDidMount Method');
     setTimeout(() => {
       this.setState({
-        name: 'componentDidMount Method',
+        name: 'from current state',
       });
     }, 5000);
   }
-  //=========Previous state
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log('getSnapshotBeforeUdpate Method');
-    return (document.getElementById('previous-state').innerHTML =
-      'The previous state was ' + prevState.name);
-  }
+
   //==========Current State
-  componentDidUpdate() {
-    console.log('componentDidUpdate Method');
-    document.getElementById('current-state').innerHTML =
-      'This current state was ' + this.state.name;
+  componentDidUpdate(prevState) {
+    console.log('componentDidUpdate Method final ender');
+    if (prevState.name != this.state.name)
+      document.getElementById('state-change').innerHTML =
+        'Yes, the state is changed';
   }
   render() {
     console.log('This method is responsible to render our JSX to DOM');
@@ -41,8 +33,7 @@ export class ChildComponent extends React.Component {
     return (
       <div>
         <h1>This is {name}</h1>
-        <p id="previous-state"></p>
-        <p id="current-state"></p>
+        <p id="state-change"></p>
       </div>
     );
   }
